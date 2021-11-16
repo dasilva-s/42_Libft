c024851b1659fddf085100262c8c9e82d8e639f5
Steph <dasilva_s@ik.me>
2021-11-16 12:55:51 +0100

    ft_strnstr v2

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,11 @@
 /*   By: stde-alm <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:47:52 by stde-alm          #+#    #+#             */
-/*   Updated: 2021/11/16 11:44:31 by stde-alm         ###   ########.fr       */
+/*   Updated: 2021/11/16 12:53:56 by stde-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"stdlib.h"
+#include"libft.h"
 
 char	*ft_strnstr( const char *s1, const char *s2, size_t l)
 {
@@ -19,19 +19,21 @@ char	*ft_strnstr( const char *s1, const char *s2, size_t l)
 
 	j = 0;
 	t = 0;
+	if (ft_strlen(s2) == 0)
+		return ((char *)s1);
 	while (*s1 && l--)
 	{
 		if (*s1 == s2[j])
 		{
 			t = 1;
-			while (s2[j + 1] != '\0' && t == 1)
+			while (s2[j] != '\0' && t == 1)
 			{
-				j++;
 				if (s1[j] != s2[j] || l - j < 0)
 				{
 					t = 0;
 					j = 0;
 				}
+				j++;
 			}
 			if (t == 1)
 				return ((char *)s1);
--- test_ft_strnstr.c
+++ test_ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: stde-alm <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:47:52 by stde-alm          #+#    #+#             */
-/*   Updated: 2021/11/16 12:12:24 by stde-alm         ###   ########.fr       */
+/*   Updated: 2021/11/16 12:49:51 by stde-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,21 @@ char	*ft_strnstr( const char *s1, const char *s2, size_t l)
 
 	j = 0;
 	t = 0;
+	if (strlen(s2) == 0)
+		return ((char *)s1);
 	while (*s1 && l--)
 	{
 		if (*s1 == s2[j])
 		{
 			t = 1;
-			while (s2[j + 1] != '\0' && t == 1)
+			while (s2[j] != '\0' && t == 1)
 			{
-				j++;
-				if (s1[j] != s2[j] || l - j <= 0)
+				if (s1[j] != s2[j] || l - j < 0)
 				{
 					t = 0;
 					j = 0;
 				}
+				j++;
 			}
 			if (t == 1)
 				return ((char *)s1);
@@ -48,8 +50,8 @@ char	*ft_strnstr( const char *s1, const char *s2, size_t l)
 int	main(void)
 {
 	char	a[50] = "AAAAAAAAAAAAA";
-	char	b[20] = "AAAAAAAAAAAAA";
-	int		l = strlen(a);
+	char	b[20];
+	int		l =0;
 	char	*c;
 
 	char	d[50] = "MZIRIBMZIRIBMZE123";
