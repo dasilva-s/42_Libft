97261350afc89700393498d4f7ca911a42952adf
Steph <dasilva_s@ik.me>
2021-11-18 11:17:57 +0100

    ft_strnstr ok!!!

--- ft_strnstr2.c
+++ ft_strnstr2.c
@@ -1,30 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stde-alm <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/17 14:12:15 by stde-alm          #+#    #+#             */
-/*   Updated: 2021/11/18 11:16:23 by stde-alm         ###   ########.fr       */
+/*   Created: 2021/11/15 16:47:52 by stde-alm          #+#    #+#             */
+/*   Updated: 2021/11/18 10:31:35 by stde-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-char	*ft_strnstr( const char *s1, const char *s2, size_t l)
+char	*ft_strnstr2( const char *s1, const char *s2, size_t l)
 {
-	size_t	l_s2;
+	int	j;
+	int	t;
 
-	l_s2 = ft_strlen(s2);
-	if (l_s2 == 0)
+	j = 0;
+	t = 0;
+	if (ft_strlen(s2) == 0)
 		return ((char *)s1);
-	while (*s1 && l >= l_s2)
+	while (*s1 && l--)
 	{
-		if (ft_strncmp((char *)s1, (char *)s2, l_s2) == 0)
-			return ((char *)s1);
+		if (*s1 == s2[j])
+		{
+			t = 1;
+			while (s2[j] != '\0' && t == 1)
+			{
+				if (s1[j] != s2[j] || l - j < 0)
+				{
+					t = 0;
+					j = 0;
+				}
+				j++;
+			}
+			if (t == 1)
+				return ((char *)s1);
+		}
 		s1++;
-		l--;
 	}
 	return (NULL);
 }
--- test_strnstr.c
+++ test_strnstr.c
@@ -6,29 +6,27 @@
 /*   By: stde-alm <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 14:12:15 by stde-alm          #+#    #+#             */
-/*   Updated: 2021/11/17 14:12:15 by stde-alm         ###   ########.fr       */
+/*   Updated: 2021/11/18 11:11:57 by stde-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 #include"ft_strlen.c"
 #include"ft_strncmp.c"
-#include"ft_strnstr.c"
+#include"ft_strnstr2.c"
 
-char	*ft_strnstr2( const char *s1, const char *s2, size_t l)
+char	*ft_strnstr( const char *s1, const char *s2, size_t l)
 {
-	int j;
-	int l_s2;
-	int t;
+	size_t l_s2;
 
-	j = 0;
 	l_s2 = ft_strlen(s2);
-	t = 0;
 
 	if (l_s2 == 0)
 		return ((char *)s1);
-	while (*s1 && l - l_s2 > 0)
+	while (*s1 && l >= l_s2 )
 	{
+		//if (l - l_s2 < 0)
+		//		break;
 		if (ft_strncmp((char *)s1, (char *)s2, l_s2) == 0)
 			return((char *)s1);
 	s1++;
@@ -41,7 +39,7 @@ int	main(void)
 {
 	char	a[50] = "AAAAAAAAAAAAA";
 	char	b[20];
-	int		l =0;
+	int		l =ft_strlen(a);
 	char	*c;
 
 	char	d[50] = "MZIRIBMZIRIBMZE123";
@@ -49,9 +47,11 @@ int	main(void)
 	int		ll = ft_strlen(e);
 
 	//c = ft_strnstr(a, a, l);
-	printf("%s \n", ft_strnstr2(a, b, l));
-	printf("%s \n \n", ft_strnstr(a, b, l));
+	printf("%s \n", ft_strnstr(a, a, l));
+	printf("%s \n", ft_strnstr2(a, a, l));
+	printf("%s \n \n", strnstr(a, a, l));
 
+	printf("%s \n", ft_strnstr(d, e, ll));
 	printf("%s \n", ft_strnstr2(d, e, ll));
-	printf("%s \n \n", ft_strnstr(d, e, ll));
+	printf("%s \n \n", strnstr(d, e, ll));
 }
