9ccffc80354c51de0cdb1068d648acf8b58c8b32
Stéph <dasilva_s@ik.me>
2021-11-25 23:54:14 +0100

    make ft_striteri

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stde-alm <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 23:17:24 by stde-alm          #+#    #+#             */
+/*   Updated: 2021/11/25 23:40:40 by stde-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	size_t			l;
+	unsigned int	i;
+
+	i = 0;
+	l = ft_strlen(s);
+	if (!s)
+		return (0);
+	str = malloc((l + 1) * sizeof(char));
+	while (str[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
